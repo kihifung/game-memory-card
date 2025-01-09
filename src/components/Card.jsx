0a1f8e58c23029b2e7b53193e6bca694a2fa11ec
Chun-Hsiang Yu <kyle.yu@tpisoftware.com>
2025-01-09 15:32:37 +0800

    add Flip Effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
+import "./Card.css";
+
 const Card = ({ card }) => (
-  <div>
-    <img src={card.src} alt="cat pic" width="200"></img>
-    <img src="rear.svg" alt="card back" width="200" />
+  <div className="card">
+    <img src="rear.svg" alt="card back" width="200" className="rear" />
+    <img src={card.src} alt="cat pic" width="200" className="front" />
   </div>
 );
 
